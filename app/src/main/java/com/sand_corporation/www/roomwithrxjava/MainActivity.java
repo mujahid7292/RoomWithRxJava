f1e446c7e6ffdf4e423ff3b4b5fbb665671e2e30
= <=>
2018-10-28 12:09:01 +0600

    Initial Commit WithoutMVVMDataBinding

--- app/src/main/java/com/sand_corporation/www/roomwithrxjava/MainActivity.java
+++ app/src/main/java/com/sand_corporation/www/roomwithrxjava/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements DataCallBack {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //WithoutMVVMDataBinding
         dataManager = new DataManager(this);
 
         btnGetData = findViewById(R.id.btnGetData);
